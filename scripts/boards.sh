474fbed89c05ee2679bc1c6c0a2998a2ae345d06
Alejandro Mery <amery@geeks.cl>
2012-10-21 00:22:49 +0200

    build: generate chosen_board.mk

--- scripts/boards.sh
+++ scripts/boards.sh
@@ -21,5 +21,36 @@ generate_boards_mk() {
 EOT
 }
 
-generate_boards_mk > boards.mk~
-mv boards.mk~ boards.mk
+# keep the output `sh` friendly
+# i.e., no spaces around the '='
+generate_chosen_board_mk() {
+	local board="${1%-android}" android= soc=
+
+	[ "$board" = "$1" ] || android=yes
+	soc=$(ls -1 sunxi-boards/sys_config/*/$board.fex 2> /dev/null | cut -d/ -f3)
+
+	cat <<-EOT
+	BOARD=$board
+	ANDROID=$android
+	SOC=$soc
+	UBOOT_CONFIG=$board
+	EOT
+
+	if [ "$soc" = a10 ]; then
+		echo "KERNEL_CONFIG=sun4i${android:+_crane}_defconfig"
+	else
+		echo "KERNEL_CONFIG=a13${android:+_nuclear}_defconfig"
+	fi
+}
+
+if [ $# -eq 0 ]; then
+	out=board.mk
+	generate_boards_mk > $out~
+elif ls -1 sunxi-boards/sys_config/*/${1%-android}.fex 2> /dev/null 1>&2; then
+	out=chosen_board.mk
+	generate_chosen_board_mk $1 > $out~
+else
+	echo "$1: invalid board name" >&2
+	exit 1
+fi
+mv $out~ $out
