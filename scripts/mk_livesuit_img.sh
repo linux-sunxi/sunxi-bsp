3ffe8fba18acba83d32837104c1e4c40c269985d
Linux Sunxi <sunxi@templatevm.jotti.com.au>
2012-11-06 09:02:41 +1100

    mk_livesuit_img.sh: improvements

--- scripts/mk_livesuit_img.sh
+++ scripts/mk_livesuit_img.sh
@@ -30,58 +30,6 @@ modify_image_cfg()
 #    echo "" >> image.cfg
 }
 
-do_pack_linux()
-{
-    echo "!!!Packing for linux!!!\n"
-
-#    if [ $PACK_CHIP = sun4i ]; then
-#	if [ $PACK_DEBUG = card0 ]; then
-#	    cp $TOOLS_DIR/awk_debug_card0 out/awk_debug_card0
-#	    TX=`awk  '$0~"a10"{print $2}' pctools/linux/card_debug_pin`
-#	    RX=`awk  '$0~"a10"{print $3}' pctools/linux/card_debug_pin`
-#	    sed -i s'/uart_debug_tx =/uart_debug_tx = '$TX'/g' out/awk_debug_card0
-#	    sed -i s'/uart_debug_rx =/uart_debug_rx = '$RX'/g' out/awk_debug_card0
-#	    sed -i s'/uart_tx =/uart_tx = '$TX'/g' out/awk_debug_card0
-#	    sed -i s'/uart_rx =/uart_rx = '$RX'/g' out/awk_debug_card0
-#	    awk -f out/awk_debug_card0 out/sys_config1.fex > out/a.fex
-#	    rm out/sys_config1.fex
-#	    mv out/a.fex out/sys_config1.fex
-#	    echo "uart -> card0 !!!"
-#	fi
-#    fi
-
-
-#    busybox unix2dos sys_config1.fex
-#    busybox unix2dos sys_config.fex
-	${BINS}/script ${LIVESUIT_DIR}/default/sys_config.fex
-	${SUNXI_TOOLS}/fex2bin sunxi-boards/sys_config/${SOC}/${BOARD}.fex > ${CONFIGS_DIR}/sys_config1.bin
-	${BINS}/update_23 ${CONFIGS_DIR}/sys_config1.bin ${CONFIGS_DIR}/boot0.bin ${CONFIGS_DIR}/boot1.bin
-	${BINS}/update_23 ${CONFIGS_DIR}/sys_config1.bin ${CONFIGS_DIR}/card_boot0.fex ${CONFIGS_DIR}/card_boot1.fex SDMMC_CARD
-
-
-	${BINS}/update_mbr ${CONFIGS_DIR}/sys_config.bin ${CONFIGS_DIR}/mbr.fex 4 16777216
-	${FSBUILD} ${CONFIGS_DIR}/bootfs.ini ${CONFIGS_DIR}/split_xxxx.fex
-	mv bootfs.fex ${CONFIGS_DIR}/bootloader.fex
-
-#    # get env.fex
-	${BINS}/u_boot_env_gen ${LIVESUIT_DIR}/default/env.cfg ${CONFIGS_DIR}/env.fex
-
-#    # get the boot.img
-#    ln -s $LICHEE_OUT/boot.img ${CONFIGS_DIR}/boot.fex
-
-#    # get the rootfs
-#    ln -s $LICHEE_OUT/rootfs.ext4 ${CONFIGS_DIR}/rootfs.fex
-
-#	do_addchecksum
-
-
-    ${DRAGON} ${LIVESUIT_DIR}/image.cfg
-
-    if [ -e ${IMG_NAME} ]; then
-        echo '---------' ${IMG_NAME} ' done-------------'
-    fi
-
-}
 
 do_addchecksum()
 {
@@ -92,6 +40,38 @@ do_addchecksum()
 	${BINS}/FileAddSum ${CONFIGS_DIR}/boot.fex ${CONFIGS_DIR}/vboot.fex
 	${BINS}/FileAddSum ${CONFIGS_DIR}/system.fex ${CONFIGS_DIR}/vsystem.fex
 	${BINS}/FileAddSum ${CONFIGS_DIR}/recovery.fex ${CONFIGS_DIR}/vrecovery.fex
+}
+
+make_boot0_boot1()
+{
+	cp -rf ${LIVESUIT_DIR}/eGon/storage_media/nand/boot0.bin \
+		${LIVESUIT_DIR}/eGon/storage_media/nand/boot1.bin \
+		${CONFIGS_DIR}
+	cp -rf ${LIVESUIT_DIR}/eGon/storage_media/sdcard/boot0.bin ${CONFIGS_DIR}/card_boot0.fex
+	cp -rf ${LIVESUIT_DIR}/eGon/storage_media/sdcard/boot1.bin ${CONFIGS_DIR}/card_boot1.fex
+
+	${BINS}/update_23 ${CONFIGS_DIR}/sys_config1.bin ${CONFIGS_DIR}/boot0.bin ${CONFIGS_DIR}/boot1.bin
+	${BINS}/update_23 ${CONFIGS_DIR}/sys_config1.bin ${CONFIGS_DIR}/card_boot0.fex ${CONFIGS_DIR}/card_boot1.fex SDMMC_CARD
+
+	${BINS}/update_mbr ${CONFIGS_DIR}/sys_config.bin ${CONFIGS_DIR}/mbr.fex 4 16777216
+	${FSBUILD} ${CONFIGS_DIR}/bootfs.ini ${CONFIGS_DIR}/split_xxxx.fex
+	mv bootfs.fex ${CONFIGS_DIR}/bootloader.fex
+
+	# get env.fex
+	${BINS}/u_boot_env_gen ${LIVESUIT_DIR}/default/env.cfg ${CONFIGS_DIR}/env.fex
+
+}
+
+make_sys_configs()
+{
+	#busybox unix2dos sys_config1.fex
+	#busybox unix2dos sys_config.fex
+	cp ${LIVESUIT_DIR}/default/sys_config.fex ${CONFIGS_DIR}/sys_config.fex
+	${BINS}/script ${CONFIGS_DIR}/sys_config.fex
+
+	cp sunxi-boards/sys_config/${SOC}/${BOARD}.fex ${CONFIGS_DIR}/sys_config1.fex
+	${SUNXI_TOOLS}/fex2bin ${CONFIGS_DIR}/sys_config1.fex > ${CONFIGS_DIR}/sys_config1.bin
+
 }
 
 make_boot_img()
@@ -138,6 +118,38 @@ make_rootfs()
 	sudo sudo rm -rf $target
 	mv linux.ext4 ${CONFIGS_DIR}/rootfs.fex
 }
+
+do_pack_linux()
+{
+    echo "!!!Packing for linux!!!\n"
+
+#    if [ $PACK_CHIP = sun4i ]; then
+#	if [ $PACK_DEBUG = card0 ]; then
+#	    cp $TOOLS_DIR/awk_debug_card0 out/awk_debug_card0
+#	    TX=`awk  '$0~"a10"{print $2}' pctools/linux/card_debug_pin`
+#	    RX=`awk  '$0~"a10"{print $3}' pctools/linux/card_debug_pin`
+#	    sed -i s'/uart_debug_tx =/uart_debug_tx = '$TX'/g' out/awk_debug_card0
+#	    sed -i s'/uart_debug_rx =/uart_debug_rx = '$RX'/g' out/awk_debug_card0
+#	    sed -i s'/uart_tx =/uart_tx = '$TX'/g' out/awk_debug_card0
+#	    sed -i s'/uart_rx =/uart_rx = '$RX'/g' out/awk_debug_card0
+#	    awk -f out/awk_debug_card0 out/sys_config1.fex > out/a.fex
+#	    rm out/sys_config1.fex
+#	    mv out/a.fex out/sys_config1.fex
+#	    echo "uart -> card0 !!!"
+#	fi
+#    fi
+
+	make_sys_configs
+	make_boot0_boot1
+
+	${DRAGON} ${LIVESUIT_DIR}/image.cfg
+
+	if [ -e ${IMG_NAME} ]; then
+		echo '---------' ${IMG_NAME} ' done-------------'
+	fi
+
+}
+
 
 #make_rootfs "$1"
 #make_boot_img
